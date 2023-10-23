646ff8bc4bb2d28b4e36a5e8e14d40dba8ecbe3f
kostadinalmishev <kostadinalmishev@gmail.com>
2023-10-23 22:53:23 +0300

    Added sample code

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,9 +1,14 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,28 +17,53 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
+    var firstText by remember { mutableStateOf("") }
+    var secondText by remember { mutableStateOf("") }
+    var thirdText by remember { mutableStateOf("") }
+    val localFocusManager = LocalFocusManager.current
+
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    contentDescription = "Compose Multiplatform icon"
-                )
-            }
+        Column(Modifier.fillMaxWidth().padding(top = 40.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            OutlinedTextField(
+                value = firstText,
+                label = {
+                    Text("one")
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        localFocusManager.moveFocus(FocusDirection.Down)
+                    }
+                ),
+                singleLine = true,
+                onValueChange = { firstText = it }
+            )
+            OutlinedTextField(
+                value = secondText,
+                label = {
+                    Text("two")
+                },
+                singleLine = true,
+                onValueChange = { secondText = it }
+            )
+            OutlinedTextField(
+                value = thirdText,
+                label = {
+                    Text("three")
+                },
+                singleLine = true,
+                onValueChange = { thirdText = it }
+            )
         }
     }
 }
--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,10 @@
+import androidx.compose.ui.uikit.OnFocusBehavior
 import androidx.compose.ui.window.ComposeUIViewController
 
 actual fun getPlatformName(): String = "iOS"
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = {
+    onFocusBehavior = OnFocusBehavior.DoNothing
+}) {
+    App()
+}
